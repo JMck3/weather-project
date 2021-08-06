d4f735b8a92a85fba2b70a23aa45f2dd904eaef6
JMck3 <themckeerings@gmail.com>
2021-08-07 00:12:51 +1000

    Assignment work 7 August

--- weather_project/starter/weather.py
+++ weather_project/starter/weather.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+from types import DynamicClassAttribute
 
 DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
 
@@ -42,9 +43,6 @@ def convert_f_to_c(temp_in_farenheit):
 def calculate_mean(weather_data):
     addition = 0
     for temp in weather_data:
-        #temp = 49
-        #temp = 57
-        #addition variable does not exist
         addition = (addition +  temp)
     print (addition/int(len(weather_data)))  
 
@@ -57,18 +55,27 @@ def calculate_mean(weather_data):
     """
     
 def load_data_from_csv(csv_file):
-    def load_data_from_csv(csv_file):
-        data = []
-    with open(csv_file) as file:
-        reader = csv.reader(file)
+    data = []
+    with open("tests/data/example_one.csv") as csv_file:
+        reader = csv.reader(csv_file)
         next(reader)
         for line in reader:
             if line != []:
-                data.append(line [1])
-    print(data)
-    return data
+                data.append(line)
+    with open("tests/data/example_two.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for line in reader:
+            if line != []:
+                data.append(line)
+    with open("tests/data/example_two.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for line in reader:
+            if line != []:
+                data.append(line)
 # need to make int line.  
-    # print (csv_file)
+
     """Reads a csv file and stores the data in a list.
 
     Args:
@@ -76,37 +83,81 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
 
 def find_min(weather_data):
-    # low: weather_data.index(min(weather_data))
-    min_value = min(weather_data)
-    if weather_data.count(min_value) > 1:
-        return [i for i, x in enumerate(weather_data) if x == min(weather_data)]
-    else:
-        position = weather_data.index(min(weather_data))
-        return (f"({min_value}, {position})"),".1f"
+    if weather_data != []:
+        minimum_value = min(weather_data)
+    position: None
+    # else:
+    #     ()
+    for pos, elem in enumerate(weather_data):
+            position = pos
+            if minimum_value == (float(elem)):
+                return(float(minimum_value), position)
+        # else:
+        #     ()
+
+    # if weather_data != []:
+    #         for count, temperature in enumerate(weather_data): #count = 0, tempature = first_elem
+    #             minvalue = min(weather_data)
+    #             # minpos = weather_data.index(min(weather_data))
+
+    #             # minvalue = min(weather_data)
+    #             # index = weather_data.index(minvalue)
+    #             # return (minvalue, index)
+    #             print (weather_data)           
+                # print(count, temperature)
+            # minimum_value = "X"
+                # position = weather_data.index(min(minvalue))
+        # weather_data = [1,2,3]
+        # minvalue = min(weather_data)
+        # minvalue = 1.5
+        # [1,2,3].index(2) = 1
+        # weather_data = [x,y,z] 
+        # pos = weather_data.index(z)
+            # position = weather_data.index(minvalue)
+        # weather_data.index(min(weather_data))
+        # return(minvalue, position)
+    # else:
+    #     return ()  
+
+    # min_value = min((weather_data))
+    # # position = weather_data.index(weather_data)
+    # print (min_value)     
 
 
-    """Calculates the minimum value in a list of numbers.
 
-    Args:
-        weather_data: A list of numbers.
-    Returns:
-        The minium value and it's position in the list.
-    """
-    pass
+
+    #     ""Calculates the minimum value in a list of numbers.
+
+    # Args:
+    #     weather_data: A list of numbers.
+    # Returns:
+    #     The minium value and it's position in the list.
+    # """
 
 
 def find_max(weather_data):
-    """Calculates the maximum value in a list of numbers.
+        if weather_data != []:
+            minimum_value = max(weather_data)
+        position: None
+    # else:
+    #     ()
+        for pos, elem in enumerate(weather_data):
+            position = pos
+            if minimum_value == (float(elem)):
+                return(float(minimum_value), position)
+        # else:
+        #     ()
 
-    Args:
-        weather_data: A list of numbers.
-    Returns:
-        The maximum value and it's position in the list.
-    """
-    pass
+
+    # """Calculates the maximum value in a list of numbers.
+
+    # Args:
+    #     weather_data: A list of numbers.
+    # Returns:
+    #     The maximum value and it's position in the list.
+    # """
 
 
 def generate_summary(weather_data):
@@ -117,10 +168,12 @@ def generate_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+
 
 
 def generate_daily_summary(weather_data):
+
+
     """Outputs a daily summary for the given weather data.
 
     Args:
@@ -128,4 +181,4 @@ def generate_daily_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+
